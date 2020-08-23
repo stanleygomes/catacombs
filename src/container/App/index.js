18719f6960bc86331bf7202140483ea3f64ea11e
Stanley <stanleygomesdasilva@gmail.com>
2020-08-23 11:05:07 -0300

    react expo icons

--- src/container/App/index.js
+++ src/container/App/index.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 
 const styles = StyleSheet.create({
   container: {
@@ -15,6 +16,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Ionicons name="md-checkmark-circle" size={32} color="green" />
       <StatusBar style="auto" />
     </View>
   );
