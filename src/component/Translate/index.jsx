cb86598b974b378a71a8e080c4b3d65f3a51e52d
Stanley <stanleygomesdasilva@gmail.com>
2020-08-28 08:17:02 -0300

    translate component

---
+++ src/component/Translate/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Text } from 'react-native';
+import PropTypes from 'prop-types';
+import { useTranslation } from 'react-i18next';
+
+const Translate = props => {
+  const { t } = useTranslation();
+  const { k } = props;
+
+  return <Text>{t(k)}</Text>;
+};
+
+Translate.propTypes = {
+  k: PropTypes.string.isRequired,
+};
+
+export default Translate;
