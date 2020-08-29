98beed2db709f2c05b921a217c6d6eb137de20cd
Stanley <stanleygomesdasilva@gmail.com>
2020-08-29 15:59:49 -0300

    Start app with nodevader basic

---
+++ README.md
@@ -0,0 +1,5 @@
+# Gitlab / Slack Automate
+
+Automação para notificações do gitlab em um canal do slack.
+
+Built with [Nodevader](https://github.com/stanleygomes/nodevader)
---
+++ src/config/index.js
@@ -0,0 +1,32 @@
+const path = require('path')
+const dotenv = require('dotenv')
+dotenv.config()
+
+const appConfig = {
+  server: {
+    port: process.env.HTTP_PORT || 3000,
+    static: 'src/static'
+  },
+  upload: {
+    maxSize: 5242880 // 5 * 1024 * 1024 = no larger than 5mb, you can change as needed.
+  },
+  baseEndpoint: '/',
+  cors: {
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204
+  },
+  i18n: {
+    locales: [
+      'pt-BR',
+      'en'
+    ],
+    defaultLocale: 'pt-BR',
+    directory: path.join(__dirname, '/i18n-locales'),
+    autoReload: true
+  },
+  privateKey: process.env.APP_KEY
+}
+
+module.exports = appConfig
