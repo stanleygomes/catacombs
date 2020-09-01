c0dc712d0639983e6382749580aa05315e352428
Stanley <stanleygomesdasilva@gmail.com>
2020-09-01 09:21:32 -0300

    ajustes merge request

--- src/api/gitlab/gitlabService.js
+++ src/api/gitlab/gitlabService.js
@@ -9,13 +9,18 @@ const hook = (req, res) => {
     const projectName = body.project.name
     const repositoryUrl = body.project.homepage
     let textTemplate = null
+    let request = null
+
+    console.log(body);
 
     if (action === 'merge_request') {
       const branchSource = body.object_attributes.source_branch
       const branchTarget = body.object_attributes.target_branch
       const iid = body.object_attributes.iid
-      // open: unchecked
-      // const mergeStatus = body.object_attributes.merge_status
+      const mergeStatus = body.object_attributes.merge_status
+      const title = body.object_attributes.title
+
+      // open:
       textTemplate = `
 Tem merge request novo para aprovar no projeto *${projectName}*, dá uma olhada aqui nesse link:
 
@@ -23,18 +28,23 @@ ${repositoryUrl}/merge_requests/${iid}
 
 Branch origem: *${branchSource}*
 Branch destino: *${branchTarget}*
+Mensagem: *${title}*
 
 Vou comprar um chocolate para quem validar! :harold: :morumbi: :araxa:
         `
+
+      if (mergeStatus === 'unchecked') {
+        request = {
+          text: textTemplate
+        }
+      }
     }
 
-    const request = {
-      text: textTemplate
+    if (request != null) {
+      http.post(slackHookUrl, request).then(resp => {
+        resolve(resp)
+      }).catch(err => reject(err))
     }
-
-    http.post(slackHookUrl, request).then(resp => {
-      resolve(resp)
-    }).catch(err => reject(err))
   })
 }
 
