615ed9628eec90054f9018680cded64540f90fb5
stanley <stanleygomesdasilva@gmail.com>
2020-09-25 20:50:05 -0300

    remove style and add config i18n

--- src/common/i18n/index.js
+++ src/common/i18n/index.js
@@ -1,6 +1,5 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
 import enUS from './locales/en.json';
 import ptBR from './locales/pt-br.json';
 
@@ -13,16 +12,13 @@ const resources = {
   },
 };
 
-i18n
-  .use(LanguageDetector)
-  .use(initReactI18next)
-  .init({
-    resources,
-    fallbackLng: 'pt-BR',
-    debug: process.env.NODE_ENV === 'development',
-    interpolation: {
-      escapeValue: false,
-    },
-  });
+i18n.use(initReactI18next).init({
+  resources,
+  fallbackLng: 'pt-BR',
+  debug: process.env.NODE_ENV === 'development',
+  interpolation: {
+    escapeValue: false,
+  },
+});
 
 export default i18n;
