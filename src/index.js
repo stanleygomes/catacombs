f0003dce65150d03920411d1bdb58c0682e8c45f
stanley <stanleygomesdasilva@gmail.com>
2020-09-25 20:49:45 -0300

    remove bootstrap

--- src/index.js
+++ src/index.js
@@ -2,6 +2,5 @@ import registerRootComponent from 'expo/build/launch/registerRootComponent';
 import App from './container/App';
 
 import './common/i18n';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 registerRootComponent(App);
