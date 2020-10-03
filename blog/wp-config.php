8db38398509a6d6b0ef15d47c9932e07579cebc1
stanley <stanleygomesdasilva@gmail.com>
2020-10-03 15:32:21 -0300

    fix ssl admin

--- blog/wp-config.php
+++ blog/wp-config.php
@@ -18,17 +18,6 @@
  * @package WordPress
  */
 
-
-/** SSL */
-define('FORCE_SSL_ADMIN', true);
-
-// in some setups HTTP_X_FORWARDED_PROTO might contain
-// a comma-separated list e.g. http,https
-// so check for https existence
-if (strpos($_SERVER['HTTP_X_FORWARDED_PROTO'], 'https') !== false) {
-  $_SERVER['HTTPS'] = 'on';
-}
-
 define('FS_METHOD', 'direct');
 
 // ** MySQL settings - You can get this info from your web host ** //
