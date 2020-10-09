b0d9963e564c976a181291e1432671b1a812451c
stanley <stanleygomesdasilva@gmail.com>
2020-10-09 06:53:27 -0300

    text plain in H1 and H3

--- src/component/H3/index.jsx
+++ src/component/H3/index.jsx
@@ -1,21 +1,29 @@
 import React from 'react';
+import { Text } from 'react-native';
 import PropTypes from 'prop-types';
 import customStyle from './style';
 import Translate from '../Translate';
 
 const H3 = props => {
-  const { text, style } = props;
+  const { text, textPlain, style } = props;
 
-  return <Translate k={text} style={{ ...customStyle.default, ...style }} />;
+  return textPlain != null ? (
+    <Text style={{ ...customStyle.default, ...style }}>{textPlain}</Text>
+  ) : (
+    <Translate k={text} style={{ ...customStyle.default, ...style }} />
+  );
 };
 
 H3.defaultProps = {
   style: null,
+  text: null,
+  textPlain: null,
 };
 
 H3.propTypes = {
-  text: PropTypes.string.isRequired,
+  text: PropTypes.string,
   style: PropTypes.object,
+  textPlain: PropTypes.string,
 };
 
 export default H3;
