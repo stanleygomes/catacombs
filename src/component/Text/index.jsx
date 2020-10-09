96a92db05488b89c3d16b42c9de4c60de0f9b792
stanley <stanleygomesdasilva@gmail.com>
2020-10-09 06:53:55 -0300

    text component

---
+++ src/component/Text/index.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Text as RNText } from 'react-native';
+import PropTypes from 'prop-types';
+import customStyle from './style';
+import Translate from '../Translate';
+
+const Text = props => {
+  const { textKey, textPlain, style } = props;
+
+  return textPlain != null ? (
+    <RNText style={{ ...customStyle.default, ...style }}>{textPlain}</RNText>
+  ) : (
+    <Translate k={textKey} style={{ ...customStyle.default, ...style }} />
+  );
+};
+
+Text.defaultProps = {
+  style: null,
+  textKey: null,
+  textPlain: null,
+};
+
+Text.propTypes = {
+  textKey: PropTypes.string,
+  style: PropTypes.object,
+  textPlain: PropTypes.string,
+};
+
+export default Text;
---
+++ src/component/Text/style.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native';
+import config from '../../common/config';
+
+const style = StyleSheet.create({
+  default: {
+    fontFamily: config.defaultFontFamilyBold,
+  },
+});
+
+export default style;
