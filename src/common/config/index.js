1a5b652a450f2e114c4a6e62fae341d718677335
stanley <stanleygomesdasilva@gmail.com>
2020-10-10 09:00:27 -0300

    theme fixes

--- src/common/config/index.js
+++
@@ -1,6 +0,0 @@
-const config = {
-  defaultFontFamily: 'Poppins_400Regular',
-  defaultFontFamilyBold: 'Poppins_700Bold',
-};
-
-export default config;
--- src/common/style/theme.js
+++ src/common/style/theme.js
@@ -1,13 +1,21 @@
 const theme = {
+  font: {
+    defaultFontFamily: 'Poppins_400Regular',
+    defaultFontFamilyBold: 'Poppins_700Bold',
+  },
   light: {
     backgroundPrimary: '#fff',
     primary: '#ef6c00',
+    primaryLight: '#f3d1b5',
     secondary: '#eba469',
+    terciary: '#b53d00',
   },
   dark: {
     backgroundPrimary: '#333',
     primary: '#ef6c00',
+    primaryLight: '#f3d1b5',
     secondary: '#eba469',
+    terciary: '#b53d00',
   },
 };
 
