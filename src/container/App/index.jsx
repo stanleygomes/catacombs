f3b92533be495487891de4af9714bda23c521295
stanley <stanleygomesdasilva@gmail.com>
2020-10-11 16:09:27 -0300

    simplify App container page

--- src/container/App/index.jsx
+++ src/container/App/index.jsx
@@ -1,55 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import { SafeAreaView } from 'react-native';
 import { AppLoading } from 'expo';
 // eslint-disable-next-line camelcase
 import { Poppins_400Regular, Poppins_700Bold, useFonts } from '@expo-google-fonts/poppins';
 import AppContext from '../../provider/appContext';
-import SignIn from '../SignIn';
-import BottomAppBar from '../../component/BottomAppBar';
-import localStorage from '../../service/localStorage';
+import AppNavigation from '../NavigationApp';
+import style from './style';
 
 const App = () => {
-  const [isFirstAccess, setIsFirstAccess] = useState(true);
-  const [loading, setLoading] = useState(true);
   const [fontsLoaded] = useFonts({
     Poppins_400Regular,
     Poppins_700Bold,
   });
-
-  const firstAccess = () => {
-    localStorage
-      .get('config')
-      .then(data => {
-        if (data != null) {
-          setIsFirstAccess(false);
-        }
-
-        setLoading(false);
-      })
-      .catch(e => {
-        setLoading(false);
-        setIsFirstAccess(true);
-        console.log(e);
-      });
-  };
-
-  const ShowView = () => {
-    return isFirstAccess === true ? <SignIn /> : <BottomAppBar />;
-  };
-
-  useEffect(() => {
-    firstAccess();
-  }, []);
 
   if (!fontsLoaded) {
     return <AppLoading />;
   }
 
   return (
-    <>
+    <SafeAreaView style={style.container}>
       <StatusBar style="dark" />
-      <AppContext.Provider>{loading === false && <ShowView />}</AppContext.Provider>
-    </>
+      <AppContext.Provider>
+        <AppNavigation />
+      </AppContext.Provider>
+    </SafeAreaView>
   );
 };
 
