f521ce9504142aac12d68576ff200dbbfd38404d
stanley <stanleygomesdasilva@gmail.com>
2020-10-15 00:05:48 -0300

    about page

--- src/container/About/index.jsx
+++ src/container/About/index.jsx
@@ -1,21 +1,29 @@
 import React from 'react';
-import { ScrollView, View } from 'react-native';
+import { ScrollView, View, Linking } from 'react-native';
+import Button from '../../component/Button';
 import Image from '../../component/Image';
+import Header from '../../component/Header';
 import H1 from '../../component/H1';
-import prayerSrc from '../../asset/image/prayer-avatar.png';
+import Text from '../../component/Text';
+import logoSrc from '../../asset/image/logo.png';
 import global from '../../common/style/global';
 import style from './style';
 
 const About = () => {
-  const user = {
-    name: 'Luke Skywalker',
+  const handleOpenLink = () => {
+    const thiagoBodrukUrl = 'https://github.com/thiagobodruk/biblia';
+    Linking.openURL(thiagoBodrukUrl);
   };
 
   return (
-    <ScrollView>
+    <ScrollView style={{ ...global.containerBackground }}>
+      <Header showBackButton="yes" />
       <View style={{ ...global.container, ...style.containerTop }}>
-        <Image source={prayerSrc} width={100} height={100} />
-        <H1 textPlain={user.name} style={style.title} />
+        <Image source={logoSrc} width={100} height={100} style={{ borderRadius: 100 }} />
+        <H1 text="appName" style={style.title} />
+        <Text textKey="appDescription" style={style.description} />
+        <Text textKey="bibleCredit" style={style.credit} />
+        <Button text="bibleCreditButton" onPress={handleOpenLink} />
       </View>
     </ScrollView>
   );
