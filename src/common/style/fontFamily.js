69fb339b4fefa78838a3794bd1c00969247a8679
stanley <stanleygomesdasilva@gmail.com>
2020-10-25 22:01:06 -0300

    family font

---
+++ src/common/style/fontFamily.js
@@ -0,0 +1,9 @@
+/* eslint-disable camelcase */
+import { Poppins_400Regular, Poppins_700Bold } from '@expo-google-fonts/poppins';
+
+const fontFamily = {
+  Poppins_400Regular,
+  Poppins_700Bold,
+};
+
+export default fontFamily;
