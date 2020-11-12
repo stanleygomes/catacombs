1cde19738dc8677752975ed109bc60b8b9600ff7
stanley <stanleygomesdasilva@gmail.com>
2020-11-12 07:59:19 -0300

    app navigation fix

--- src/container/NavigationApp/index.jsx
+++ src/container/NavigationApp/index.jsx
@@ -3,18 +3,55 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import AppTabsNavigation from '../NavigationAppTabs';
 import SignIn from '../SignIn';
+import Theme from '../Theme';
+import About from '../About';
+import Privacy from '../Privacy';
+import ReadingReminder from '../ReadingReminder';
+import ProfileInfo from '../ProfileInfo';
 import AppContext from '../../provider/appContext';
 
 const { Navigator, Screen } = createStackNavigator();
 
 const NavigationApp = () => {
+  const screens = [
+    {
+      name: 'AppTabsNavigation',
+      component: AppTabsNavigation,
+    },
+    {
+      name: 'ProfileInfo',
+      component: ProfileInfo,
+    },
+    {
+      name: 'ReadingReminder',
+      component: ReadingReminder,
+    },
+    {
+      name: 'Theme',
+      component: Theme,
+    },
+    {
+      name: 'Privacy',
+      component: Privacy,
+    },
+    {
+      name: 'About',
+      component: About,
+    },
+  ];
+
   return (
     <AppContext.Consumer>
       {({ appConfig }) => (
         <NavigationContainer>
           <Navigator screenOptions={{ headerShown: false }}>
             {appConfig.signInChallenge === true && (
-              <Screen name="AppTabsNavigation" component={AppTabsNavigation} />
+              <>
+                {screens != null &&
+                  screens.map(screen => (
+                    <Screen key={screen.name} name={screen.name} component={screen.component} />
+                  ))}
+              </>
             )}
             {appConfig.signInChallenge !== true && <Screen name="SignIn" component={SignIn} />}
           </Navigator>
