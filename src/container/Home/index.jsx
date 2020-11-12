8ad22606b9ca09dafad37b3d0eb5bc5e4aa9d992
stanley <stanleygomesdasilva@gmail.com>
2020-11-12 14:09:11 -0300

    posts fix

--- src/container/Home/index.jsx
+++ src/container/Home/index.jsx
@@ -1,10 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { ScrollView, View } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import H1 from '../../component/H1';
 import ImageClickable from '../../component/ImageClickable';
 import Loading from '../../component/Loading';
 import Text from '../../component/Text';
+import ScrollViewRefresh from '../../component/ScrollViewRefresh';
 import logoSrc from '../../asset/image/logo.png';
 import AppContext from '../../provider/appContext';
 import devotionalService from '../../service/devotional';
@@ -19,7 +20,7 @@ const Home = () => {
     navigate(to);
   };
 
-  const getPosts = () => {
+  const getPosts = useCallback(() => {
     setLoading(true);
 
     devotionalService
@@ -32,7 +33,7 @@ const Home = () => {
         setLoading(false);
         throw new Error(error);
       });
-  };
+  });
 
   useEffect(() => {
     getPosts();
@@ -74,8 +75,12 @@ const Home = () => {
               />
             )}
           </View>
-          <ScrollView style={{ ...style(appConfig.theme).container }}>
-            {loading === true && <Loading theme={appConfig.theme} text="loadingDevotional" />}
+          <ScrollViewRefresh
+            style={style(appConfig.theme).container}
+            isRefreshing={loading}
+            theme={appConfig.theme}
+            onRefresh={getPosts}
+          >
             {loading === false && postsList != null && (
               <View style={style(appConfig.theme).postContainer}>
                 <ScrollView
@@ -103,7 +108,7 @@ const Home = () => {
                 </ScrollView>
               </View>
             )}
-          </ScrollView>
+          </ScrollViewRefresh>
         </>
       )}
     </AppContext.Consumer>
