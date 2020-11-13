cc4f5508d1f6576128fd28f58ba1063055da29ab
stanley <stanleygomesdasilva@gmail.com>
2020-11-13 08:16:18 -0300

    starting sqlite config to load database

--- src/container/Read/index.jsx
+++ src/container/Read/index.jsx
@@ -1,28 +1,80 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { ScrollView, View, Button, Text } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import H1 from '../../component/H1';
 import AppContext from '../../provider/appContext';
+import SwipeablePanel from '../../component/SwipeablePanel';
+import databaseService from '../../service/database';
+import filesystemService from '../../service/filesystem';
 import style from './style';
 
 const Read = () => {
-  const { goBack } = useNavigation();
+  const [isPanelActive, setIsPanelActive] = useState(false);
+
+  const openPanel = () => {
+    setIsPanelActive(true);
+  };
+
+  const closePanel = () => {
+    setIsPanelActive(false);
+  };
+
+  const { navigate } = useNavigation();
+  // const { goBack } = useNavigation();
 
   const handleNavigateToTabs = () => {
-    goBack();
+    navigate('/Profile');
+    // goBack();
   };
+
+  const connect = () => {
+    // const db = databaseService.open('name2.db');
+    // console.warn(db);
+
+    // databaseService.execute(db, 'create table if not exists items (id integer primary key not null, done int, value text);').then(r => {
+    //   console.warn('deu certo')
+    //   console.warn(r)
+    // }).catch(error => {
+    //   console.warn('deu errado')
+    //   console.warn(error)
+    // })
+
+    // separar as versoes
+    // hospedar as versoes no firebase storage
+    // criar view para download das versoes
+    // baixar versao dentro da pasta SQLite
+    // opcao para remover
+
+    // filesystemService.createFolder('SQLite').then(a => {
+      filesystemService.readFolder('SQLite').then(r => {
+        console.warn(r);
+      }).catch(error => {console.warn(error)});
+    // }).catch(b => {console.warn(b)})
+  };
+
+  useEffect(() => {
+    connect();
+  }, []);
 
   return (
     <AppContext.Consumer>
       {({ appConfig }) => (
-        <ScrollView>
-          <View style={{ ...style(appConfig.theme).container, ...{ paddingTop: 100 } }}>
-            <Button onPress={handleNavigateToTabs} title="Bom dia" theme={appConfig.theme}>
-              <Text>OLA</Text>
-            </Button>
-            <H1 textPlain="Read" style={style(appConfig.theme).title} theme={appConfig.theme} />
-          </View>
-        </ScrollView>
+        <>
+          <ScrollView style={{ ...style(appConfig.theme).container }}>
+            <View style={{ ...style(appConfig.theme).container, ...{ paddingTop: 100 } }}>
+              <Button onPress={openPanel} title="abrir painel" theme={appConfig.theme}>
+                <Text>OLA</Text>
+              </Button>
+              <Button onPress={handleNavigateToTabs} title="tabs" theme={appConfig.theme}>
+                <Text>OLA 2</Text>
+              </Button>
+              <H1 textPlain="Read" style={style(appConfig.theme).title} theme={appConfig.theme} />
+            </View>
+          </ScrollView>
+          <SwipeablePanel onClose={closePanel} isActive={isPanelActive} theme={appConfig.theme}>
+            <H1 textPlain="Home" style={style(appConfig.theme).title} theme={appConfig.theme} />
+          </SwipeablePanel>
+        </>
       )}
     </AppContext.Consumer>
   );
---
+++ src/service/filesystem.js
@@ -0,0 +1,106 @@
+import * as FileSystem from 'expo-file-system';
+import { Expo } from 'expo';
+
+const root = FileSystem.documentDirectory;
+
+const readFolder = folder => {
+  return new Promise((resolve, reject) => {
+    const directory = `${root}${folder}`;
+
+    FileSystem.readDirectoryAsync(directory)
+      .then(r => {
+        resolve(r);
+      })
+      .catch(error => reject(error));
+  });
+};
+
+const createFolder = folder => {
+  return new Promise((resolve, reject) => {
+    const directory = `${root}${folder}`;
+
+    FileSystem.getInfoAsync(directory)
+      .then(e => {
+        if (e.exists === false) {
+          FileSystem.makeDirectoryAsync(directory)
+            .then(() => {
+              resolve(true);
+            })
+            .catch(error => reject(error));
+        } else {
+          resolve(true);
+        }
+      })
+      .catch(error => reject(error));
+  });
+};
+
+const folderExists = folder => {
+  return new Promise((resolve, reject) => {
+    const directory = `${root}${folder}`;
+
+    FileSystem.getInfoAsync(directory)
+      .then(r => resolve(r.exists))
+      .catch(error => reject(error));
+  });
+};
+
+const copyToFilesystem = (asset, folder, file) => {
+  return new Promise((resolve, reject) => {
+    const directory = `${root}${folder}/${file}`;
+    const requirePath = require('../asset/default.png');
+
+    FileSystem.downloadAsync(Expo.Asset.fromModule(require('../asset/default.png')).uri, directory)
+      .then(() => {
+        // copiar o arquivo
+      })
+      .catch(error => reject(error));
+  });
+};
+
+const copyDbToFilesystem = pathToDatabaseFile => {
+  return new Promise((resolve, reject) => {
+    const sqliteFolder = 'SQLite';
+    // const dbPath = '../../sqlite/bible/default.db';
+    // const requirePath = require('../asset/default.db');
+    // const asset = Asset.fromModule(requirePath).uri;
+    const asset = null;
+
+    folderExists(sqliteFolder)
+      .then(exists => {
+        if (exists === false) {
+          createFolder(sqliteFolder)
+            .then(() => {
+              copyToFilesystem(asset, sqliteFolder, 'dboficial.db')
+                .then(() => resolve(true))
+                .catch(error => reject(error));
+            })
+            .catch(error => reject(error));
+        } else {
+          copyToFilesystem(dbPath, sqliteFolder, 'dboficial.db')
+            .then(() => resolve(true))
+            .catch(error => reject(error));
+        }
+      })
+      .catch(error => reject(error));
+  });
+
+//   if (!(await FileSystem.getInfoAsync(FileSystem.documentDirectory + 'SQLite')).exists) {
+//     await FileSystem.makeDirectoryAsync(FileSystem.documentDirectory + 'SQLite');
+//   }
+
+//   await FileSystem.downloadAsync(
+//     Asset.fromModule(require(pathToDatabaseFile)).uri,
+//     FileSystem.documentDirectory + 'SQLite/myDatabaseName.db'
+//   );
+
+//   return SQLite.openDatabase('myDatabaseName.db');
+}
+
+export default {
+  readFolder,
+  createFolder,
+  folderExists,
+  copyDbToFilesystem,
+  copyToFilesystem,
+};
