102bcfbbf9c6941364d9ace6c0b9a28abc93c84e
stanley <stanleygomesdasilva@gmail.com>
2021-05-16 21:54:59 -0300

    mostrar corpo da request

--- src/api/projectbox/projectboxRest.js
+++ src/api/projectbox/projectboxRest.js
@@ -3,6 +3,7 @@ const projectboxRest = express.Router()
 // const projectboxService = require('./projectboxService')
 
 projectboxRest.post('/hook', (req, res) => {
+  console.log(req.body)
   res.json(req.body)
   // projectboxService.hook(req, res).then(response => {
   //   res.json(response)
